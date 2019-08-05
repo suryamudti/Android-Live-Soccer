fedc130b66397f23bc829d65b88079cbf9a1d132
suryamudti <suryamudti0128@gmail.com>
2019-08-05 23:43:13 +0700

    initial update

---
+++ app/src/main/java/com/surya/livescoreleague/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.surya.livescoreleague.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.surya.livescoreleague.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
